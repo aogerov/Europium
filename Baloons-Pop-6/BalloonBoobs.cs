f6ceeb249e1ddaa50c3d93bc5372118d51fe041b
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-05-14 21:06:58 +0300

    Change

--- Baloons-Pop-6/BalloonBoobs.cs
+++ Baloons-Pop-6/BalloonBoobs.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace BalloonBoobsGame
 {
-    //Balloon Boobs It's me
+    //Balloon Boobs
     public class BalloonBoobs : Game
     {
         public void checkLeft(byte[,] matrix, int row, int column, int searchedItem)
