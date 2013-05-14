5f276e47b8be04702cacc936c454ca406478aa65
fanni-p <fanni@abv.bg>
2013-05-15 00:06:48 +0300

    Add unit tests for user input

---
+++ UnitTests/EngineClassTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BalloonBoobsGame;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EngineClassTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestInputNull()
+        {
+            string userInput = null;
+            Engine.ValidateUserInput(userInput);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TestNegativeInput()
+        {
+            string userInput = "-1 0";
+            Engine.ValidateUserInput(userInput);
+        }
+
+        [TestMethod]
+        public void TestWithInputZero()
+        {
+            string userInput = "0 0";
+            Assert.AreEqual(true,Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestWithIncorrectRows()
+        {
+            string userInput = "5 0";
+            Assert.AreEqual(false, Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestWithIncorrectCols()
+        {
+            string userInput = "0 10";
+            Assert.AreEqual(false, Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestCorrectInput()
+        {
+            string userInput = "4 9";
+            Assert.AreEqual(true, Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestWithDotSeparator()
+        {
+            string userInput = "1.5";
+            Assert.AreEqual(true, Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestWithComaSeparator()
+        {
+            string userInput = "2,6";
+            Assert.AreEqual(true, Engine.ValidateUserInput(userInput));
+        }
+
+        [TestMethod]
+        public void TestWithIncorrectSeparator()
+        {
+            string userInput = "3:6";
+            Assert.AreEqual(false, Engine.ValidateUserInput(userInput));
+        }
+    }
+}
