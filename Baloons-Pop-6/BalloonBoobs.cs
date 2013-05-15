7fb59a192f653799a1c2012b1484f7e78b86cdd7
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-05-15 12:22:00 +0300

    some comments

--- Baloons-Pop-6/BalloonBoobs.cs
+++ Baloons-Pop-6/BalloonBoobs.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 namespace BalloonBoobsGame
 {
+    //balloon boobs game
     public class BalloonBoobs : Game
     {
         private void checkLeft(byte[,] matrix, int row, int column, int searchedItem)
