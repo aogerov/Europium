6f6a79e558c599b8acfc544bfccf156e86b409a1
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-05-16 19:28:51 +0300

    Board Tests Commited

--- BaloonsTests/BoardTests.cs
+++ BaloonsTests/BoardTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Balloons;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 
 namespace BaloonsTests
 {
@@ -17,6 +18,59 @@ namespace BaloonsTests
             int[,] actual = Board.Generate(2, 2, 1, 9);
             Assert.AreEqual(expected.GetLength(0), actual.GetLength(0));
             Assert.AreEqual(expected.GetLength(1), actual.GetLength(1));
+        }
+
+        [TestMethod]
+        public void DropDownBalloonTest()
+        {
+            int[,] testMatrix = new int[,]{
+                { 4, 2, 7},
+                { 3, 3, 4},
+                { 6, 0, 4}
+            };
+
+            int[,] expected = new int[,]{
+                { 4, 0, 7},
+                { 3, 2, 4},
+                { 6, 3, 4}
+            };
+
+            int[,] actual = Board.DropDownBaloons(testMatrix);
+            for (int i = 0; i < testMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < testMatrix.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], actual[i, j]);
+                   
+                }
+            }
+        }
+
+        [TestMethod]
+        public void AsStringTest()
+        {
+            int[,] testMatrix = new int[,]{
+                { 1, 2, 3},
+                { 1, 3, 3}
+            };
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("    ");
+            sb.Append("0 1 2 ");
+            sb.Append("\r\n   ");
+            sb.Append("-------");
+            sb.Append("\r\n");
+            sb.Append("0 | 1 2 3 | ");
+            sb.Append("\r\n");
+            sb.Append("1 | 1 3 3 | ");
+            sb.Append("\r\n");
+            sb.Append("   -------\r\n");
+            string expected = sb.ToString();
+
+            string actual = Board.AsString(testMatrix);
+            
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
