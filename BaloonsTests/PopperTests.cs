31e518282f1565eee835b44957859e2fcf322439
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-05-16 21:31:37 +0300

    Popper Tests Final

--- BaloonsTests/PopperTests.cs
+++ BaloonsTests/PopperTests.cs
@@ -53,7 +53,80 @@ namespace BaloonsTests
                     Assert.AreEqual(expected[i, j], matrixTest[i, j]);
                 }
             }
-
         }
+
+        [TestMethod]
+        public void TryPopUpTest()
+        {
+            int[,] matrixTest = new int[,]{
+                { 4, 6, 5},
+                { 3, 4, 4}
+            };
+
+            Popper.TryPopUp(matrixTest, 1, 1, 6);
+
+            int[,] expected = new int[,]{
+                { 4, 0, 5},
+                { 3, 4, 4}
+            };
+
+            for (int i = 0; i < matrixTest.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixTest.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], matrixTest[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TryPopDownTest()
+        {
+            int[,] matrixTest = new int[,]{
+                { 4, 6, 5},
+                { 3, 4, 4}
+            };
+
+            Popper.TryPopDown(matrixTest, 0, 1, 4);
+
+            int[,] expected = new int[,]{
+                { 4, 6, 5},
+                { 3, 0, 4}
+            };
+
+            for (int i = 0; i < matrixTest.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixTest.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], matrixTest[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TryPopDownTest2()
+        {
+            int[,] matrixTest = new int[,]{
+                { 4, 6, 5},
+                { 3, 4, 4}
+            };
+
+            Popper.TryPopDown(matrixTest, -2, -3, 4);
+
+            int[,] expected = new int[,]{
+                { 4, 6, 5},
+                { 3, 0, 4}
+            };
+
+            for (int i = 0; i < matrixTest.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixTest.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], matrixTest[i, j]);
+                }
+            }
+        }
+        
     }
 }
